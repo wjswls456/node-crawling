fc831e3cf079b7ab67eb9bf51176b84eec7946d0
bonschicken <53087860+wjswls456@users.noreply.github.com>
2022-04-28 01:35:53 +0900

    과도한 요청으로 네이버측에서 막음 0.5초 텀을 줌

--- blogCrawler.js
+++ blogCrawler.js
@@ -19,49 +19,67 @@ let customHeaderRequest = request.defaults({
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.81 Safari/537.36",
     },
   });
+  cron.schedule("50 23 * * *", async () => {
+    let dataList = [];
 let keywordList = await models.Blogkeyword.findAll({
   raw:true,
   where:{
     status:"Y"
   }
 })
-keywordList.forEach((idx,val)=>{
+ keywordList.forEach((idx,val)=>{
+  dataList.push(idx.keyword)
+})
+let time = 100;
+dataList.forEach(async (idx,val)=>{ 
 
-  let keyword = idx.keyword
+let keyword = idx
 
 let encodeURIdata = encodeURI(keyword);
-searchUrl += encodeURIdata+"&nso=&where=blog&sm=tab_opt";
+let encodeSearchUrlData = searchUrl+encodeURIdata+"&nso=&where=blog&sm=tab_opt";
 
-
- customHeaderRequest.get(searchUrl,async (err,resp,html)=>{
-     const $ = cheerio.load(html)
-     const lst_totalLi = $(".lst_total").find("li");
+ const getRequest = (encodeSearchUrlData) =>{
+  customHeaderRequest.get(encodeSearchUrlData,async (err,resp,html)=>{
+    const $ = cheerio.load(html)
+    const lst_totalLi = $(".lst_total").find("li");
 
      for(let i =0; i < 7;i++){
-        let blogName = $(lst_totalLi[i]).find(".elss.etc_dsc_inner").text()
-        let blogUrl = $(lst_totalLi[i]).find(".sub_txt.sub_name").attr("href")
-        let mobileBlogUrl = blogUrl.replace("blog.","m.blog.")
-        await sleep(1000)
+      
+       let blogName = $(lst_totalLi[i]).find(".elss.etc_dsc_inner").text()
+       let blogUrl = $(lst_totalLi[i]).find(".sub_txt.sub_name").attr("href")
+       let mobileBlogUrl = blogUrl.replace("blog.","m.blog.")
+       
+      //  console.log("요청 전 ",new Date())
+      //  console.log(mobileBlogUrl,blogName,keyword,i,encodeSearchUrlData)
+       setTimeout(()=>{
         customHeaderRequest.get(mobileBlogUrl,async(err,resp,html)=>{
-            const $ = cheerio.load(html)
-            const count = $(".count__LOiMv").text()
-            const countArr = count.split(" ");
+          const $ = cheerio.load(html)
+          const count = $(".count__LOiMv").text()
+          const countArr = count.split(" ");
+          // console.log("요청 후 ",new Date())
+          // console.log(mobileBlogUrl,blogName,keyword,i+1)
+          const countToday = countArr[1].split(",").join("")*1
+          const countTotal = countArr[4].split(",").join("")*1
+          const countFriend = $(".buddy___ckI_").text().split("명의 이웃").join("").split(",").join("")*1
 
-            await sleep(1000)
-            const countToday = countArr[1].split(",").join("")*1
-            const countTotal = countArr[4].split(",").join("")*1
-            const countFriend = $(".buddy___ckI_").text().split("명의 이웃").join("").split(",").join("")*1
+          await models.Blogger.create({
+            keyword :keyword,
+            rank:i+1,
+            blogname:blogName,
+            url:mobileBlogUrl,
+            today : countToday,
+            total:countTotal,
+            friend:countFriend
+              })
+          })
+       },time*(i+1))
+      }
+   })
+ }
+ setTimeout(()=>{getRequest(encodeSearchUrlData)},time)
+ time +=500
+  })
 
-            await models.Blogger.create({
-              keyword :keyword,
-              rank:i+1,
-              url:mobileBlogUrl,
-              today : countToday,
-              total:countTotal,
-              friend:countFriend
-                })
-            })
-        }         
-    })
+  
   })
 })();
